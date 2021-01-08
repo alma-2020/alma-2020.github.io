59923837717128a7391b72918a3d85d145b42e25
alma-2020 <studiosjakku@gmail.com>
2021-01-07 23:04:49 -0300

    fix date.tsx

--- pages/components/date.tsx
+++ pages/components/date.tsx
@@ -6,8 +6,19 @@ interface ChildProps {
 }
 
 const Date: FC<ChildProps> = ({ dateString }) => {
-    const date = parseISO(dateString)
+    const date = parseISO(dateString);
+
+    if (!isDateValid(date)) {
+        // return an empty div if the date is invalid
+        console.warn(`Error: Invalid date ${dateString}`);
+        return <div></div>
+    }
+
     return <time dateTime={dateString}>{format(date, 'dd/MM/yyyy')}</time>
+}
+
+function isDateValid(date: Date): boolean {
+    return !isNaN(date.getTime());
 }
 
 export default Date;
