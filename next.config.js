8099509fc86d78ba91654c06eabd5ed8244ca985
alma-2020 <studiosjakku@gmail.com>
2021-01-08 00:07:23 -0300

    undo some changes

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 const PROJECT_NAME = 'alma-2020';
 const IS_PROD = process.env.NODE_ENV === 'production'
 
+/*
 module.exports = {
     assetPrefix: IS_PROD ? `/${PROJECT_NAME}/` : '',
     basePath: IS_PROD ? `/${PROJECT_NAME}` : '',
 }
+*/
