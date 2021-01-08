2161ed2f7b3fcae996579fc21ff42f52c6be3150
alma-2020 <studiosjakku@gmail.com>
2021-01-07 22:47:07 -0300

    fix next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const USERNAME = 'alma-2020';
+const IS_PROD = process.env.NODE_ENV === 'production'
 
-export default {
-    assetPrefix: `/${USERNAME}/`,
-    basePath: `/${USERNAME}`,
+module.exports = {
+    assetPrefix: IS_PROD ? `/${USERNAME}/` : '',
+    basePath: IS_PROD ? `/${USERNAME}` : '',
 }
