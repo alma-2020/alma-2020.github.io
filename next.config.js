63aeea22bb50584cb1642b767bca62e64ef8becd
alma-2020 <studiosjakku@gmail.com>
2021-01-07 23:30:57 -0300

    update build script

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
-const USERNAME = 'alma-2020';
+//const PROJECT_NAME = 'alma-2020';
 const IS_PROD = process.env.NODE_ENV === 'production'
 
 /*
 module.exports = {
-    assetPrefix: IS_PROD ? `/${USERNAME}/` : '',
-    basePath: IS_PROD ? `/${USERNAME}` : '',
+    assetPrefix: IS_PROD ? `/${PROJECT_NAME}/` : '',
+    basePath: IS_PROD ? `/${PROJECT_NAME}` : '',
 }
 */
