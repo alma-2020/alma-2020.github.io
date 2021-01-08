8aec4be29f235f92d64a404d7b41610d389d9a8b
alma-2020 <studiosjakku@gmail.com>
2021-01-07 23:59:28 -0300

    testing

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
-//const PROJECT_NAME = 'alma-2020';
+const USERNAME = 'alma-2020';
 const IS_PROD = process.env.NODE_ENV === 'production'
 
-/*
 module.exports = {
     assetPrefix: IS_PROD ? `/${PROJECT_NAME}/` : '',
     basePath: IS_PROD ? `/${PROJECT_NAME}` : '',
 }
-*/
